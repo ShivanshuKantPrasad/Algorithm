ca23b1a1b697a5037cf8d4c8866f8172df59dde1
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2018-09-23 20:11:45 +0530

    Refactor SearchingTest to use setUp

--- Tests/Searching_test.py
+++ Tests/Searching_test.py
@@ -3,10 +3,12 @@ from Searching.Linear import search
 
 class SearchingTest(unittest.TestCase):
 
+    def setUp(self):
+        self.list = [20, 30, 25, 14, 25]
+
     def testLinearSearch(self):
-        list = [20, 30, 25, 14, 25]
-        self.assertEqual(search(list, 25), 2)
-        self.assertEqual(search(list, 2), None)
+        self.assertEqual(search(self.list, 25), 2)
+        self.assertEqual(search(self.list, 2), None)
 
 if __name__ == '__main__':
     unittest.main()
